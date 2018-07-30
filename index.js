18752fef2aca936bab19877ab3cb7034bf5a6f15
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-30 15:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code in this file!
 function scuberGreetingForFeet(input){
   let result;
-  if (input <= 400){
-    result = "This one is on me!";
-  } else if ( input >= 2000 && input < 2500) {
+    if (input <= 400){
+      result = "This one is on me!";
+    } else if ( input >= 2000 && input < 2500) {
       result = "I will gladly take your thirty bucks."
     }
     else {
