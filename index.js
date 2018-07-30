2231e5af07ce1ce41fdb96d18aa3e74a2a3c8e6f
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-30 15:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+function scuberGreetingForFeet(input){
+  let result;
+  if (input <= 400){
+    result = "This oneis on me!";
+  } else if ( 400 < input <= 2000 ) {
+      result = "This will be $30"
+    }
+}
